bca663e9b7eb1bb9262cc47e1d0715cb44ddc850
Lawson Israel Pascal <isreallawson@outlook.com>
2024-07-24 15:17:58 +0000

    requirement fix and took away comments - wc requirement

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,3 @@
-export default function getResponseFromAPI () {
-  return new Promise((resolve, reject) => {
-    // Simulating an asynchronous operation
-    // Resolve the promise without any specific value
-    resolve();
-  });
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
 }
