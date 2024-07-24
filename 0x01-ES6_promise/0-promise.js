f95bab433f144f7c3def7c366d9445dfeafae574
Lawson Israel Pascal <isreallawson@outlook.com>
2024-07-24 15:09:32 +0000

    Keep every promise you make and only make promises you can keep

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,7 @@
+export default function getResponseFromAPI () {
+  return new Promise((resolve, reject) => {
+    // Simulating an asynchronous operation
+    // Resolve the promise without any specific value
+    resolve();
+  });
+}
