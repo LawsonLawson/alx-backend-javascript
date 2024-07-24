3256a14e915c595cc4c2e8ae3033863a75e2ca3e
Lawson Israel Pascal <isreallawson@outlook.com>
2024-07-24 20:26:32 +0000

    standard function to use ES6’s

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
-export default function taskBlock (trueOrFalse) {
+export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true; // eslint-disable this line
-    const task2 = false; // eslint-disable this line as well
+    const task = true;
+    const task2 = false;
   }
 
   return [task, task2];
