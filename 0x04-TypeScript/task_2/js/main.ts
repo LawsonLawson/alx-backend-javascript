2c544a89eccc3cdf25017e79bbb78db9b2a5acb2
Lawson Israel Pascal <isreallawson@outlook.com>
2024-07-31 17:08:02 +0000

    added `isDirector` function - functions for specific employeees

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -62,3 +62,52 @@ export function createEmployee(salary: number | string): Teacher | Director {
   if (typeof salary === 'number' && salary < 500) return new Teacher();
   return new Director();
 }
+
+// Task 6
+
+/**
+ * Type guard to check if an employee is a Director.
+ * @param employee - The employee to check
+ * @returns True if the employee is a Director, otherwise false
+ */
+export function isDirector(employee: TeacherInterface | DirectorInterface): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+/**
+ * Function to execute the work tasks of an employee based on their role.
+ * @param employee - The employee whose tasks are to be executed
+ * @returns The work task message
+ */
+export function executeWork(employee: DirectorInterface | TeacherInterface): string {
+  let msg: string;
+
+  if (isDirector(employee)) {
+    msg = employee.workDirectorTasks();
+  } else {
+    msg = employee.workTeacherTasks();
+  }
+
+  console.log(msg);
+  return msg;
+}
+
+// Task 7
+
+/**
+ * Type representing the subjects that can be taught.
+ */
+type Subjects = 'Math' | 'History';
+
+/**
+ * Function to teach a class based on the subject.
+ * @param todayClass - The subject of the class to be taught
+ * @returns A message indicating the class being taught
+ */
+export function teachClass(todayClass: Subjects): string | undefined {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  } else if (todayClass === 'History') {
+    return 'Teaching History';
+  }
+}
