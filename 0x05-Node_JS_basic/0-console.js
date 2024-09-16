9b3a6f01fbde93787d77ff493ea6806f070ab680
Lawson Israel Pascal <isreallawson@outlook.com>
2024-09-16 22:35:53 +0000

    a function named displayMessage that prints in STDOUT the string argument

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,13 @@
+/**
+ * Displays a message on the standard output (console).
+ *
+ * This function takes a string message as input and outputs it to the console
+ * using `console.log`.
+ *
+ * @param {string} msg - The message to display in the console.
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
